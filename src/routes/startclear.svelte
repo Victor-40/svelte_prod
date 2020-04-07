e886be827d5709417e793cbdf74d24adc2532b2e
Victor Cherezov <victor_cherezov@mentor.com>
2020-04-07 18:18:00 +0300

    grid column

--- src/routes/startclear.svelte
+++ src/routes/startclear.svelte
@@ -3,34 +3,30 @@
   import { onMount } from "svelte";
 
   const path = "http://rum-cherezov-dt:5000/api/startclear";
-  
+
   let freeVm = [];
   let snapList = [];
   let allCfg = {};
   let selectedVm = "";
   let selectedSnapshot = "";
   let snaplist = [];
-  
+
   // $: snapList = allCfg[selectedVm].snap;
 
   function test_cfg(c) {
     // console.log(c);
     for (let key in c) {
-        freeVm.push(key);
-        
-        
+      freeVm.push(key);
     }
     freeVm = freeVm;
     allCfg = c;
-    console.log(allCfg)
-  };
+    console.log(allCfg);
+  }
 
-// onMount(getTodos());
+  // onMount(getTodos());
 
-// function getTodos() {
-axios
-  .get(path)
-  .then(response  => {
+  // function getTodos() {
+  axios.get(path).then(response => {
     test_cfg(response.data);
     // allCfg = response.data;
     // allCfg = allCfg;
@@ -52,21 +48,22 @@ axios
   th {
     padding-bottom: 20px;
   }
-  .layer {
-    width: 80%;
-    margin: 0 auto;
-  }
-  h2 {
+ 
+  h3 {
     text-align: center;
     margin-bottom: 30px;
   }
-  /* ul {
-    list-style: none;
-    padding-left: 0;
-  } */
-	/* .busy {
-		color: red;
-	} */
+  .wrapper {
+    margin-top: 30px;
+    display: grid;
+    grid-template-columns: 300px 300px;
+    grid-column-gap: 50px;
+    justify-content: center;
+  }
+  .card {
+    padding:10px;
+    background-color: aquamarine;
+  }
 </style>
 
 <svelte:head>
@@ -74,34 +71,37 @@ axios
 </svelte:head>
 
 <div>
-  <h2>Start clear VM</h2>
-  <table class="layer">
-    <tr>
-      <th>Choose free VM</th>
-      <th>Choose snapshot</th>
-    </tr>
-    <tr>
-      <td>
-        {#each freeVm as free}
+ 
+  <div class="wrapper">
+    <div class="card"> 
+    <h3>Choose free VM</h3>
+      {#each freeVm as free}
           <label>
-            <input type="radio" bind:group={selectedVm} value={free}  on:change={()=>{selectedSnapshot=''; snapList = allCfg[selectedVm].snap;}} />
-          {free}
-          </label><br>
-        {/each}
-      </td>
-      <td>
-        {#if snapList}
+            <input
+              type="radio"
+              bind:group={selectedVm}
+              value={free}
+              on:change={() => {
+                selectedSnapshot = '';
+                snapList = allCfg[selectedVm].snap;
+              }} />
+            {free}
+          </label>
+          <br />
+        {/each}</div>
+    <div class="card">     
+    <h3>Choose snapshot</h3>
+     {#if snapList}
           {#each snapList as sn}
             <label>
               <input type="radio" bind:group={selectedSnapshot} value={sn} />
-            {sn}
-            </label><br>
+              {sn}
+            </label>
+            <br />
           {/each}
-        {/if}
-      </td>
-    </tr>
-  </table>
+        {/if}</div>
+  </div>
   <p>Your choice:</p>
-  <p>{selectedVm}  --  {selectedSnapshot}</p>
+  <p>{selectedVm} -- {selectedSnapshot}</p>
 
 </div>
